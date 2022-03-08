936f5a1bc188a6a2cd9cb5a43193e8b1580cdc7d
rhames07 <rhames.lima@mercadolivre.com>
2022-03-08 17:02:27 -0300

    adjusts to pom and another stuff

---
+++ src/main/java/com/github/rhames07/arithmetic/Operation.java
@@ -0,0 +1,22 @@
+package com.github.rhames07.arithmetic;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Operation {
+  public static BigDecimal add(BigDecimal x, BigDecimal y) {
+    return x.add(y);
+  }
+
+  public static BigDecimal subtract(BigDecimal x, BigDecimal y) {
+    return x.subtract(y);
+  }
+
+  public static BigDecimal multiply(BigDecimal x, BigDecimal y) {
+    return x.multiply(y);
+  }
+
+  public static BigDecimal divide(BigDecimal x, BigDecimal y) {
+    return x.divide(y, RoundingMode.UNNECESSARY);
+  }
+}
